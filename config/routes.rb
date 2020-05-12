3268685c36f9a497a74742e0137ee7ad19cfa42d
Morgan Lizop <morgan.lizop@iese.net>
2020-05-12 15:05:42 +0100

    user can now create a task and view all tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'index', to: 'tasks#index'
-  get 'answer', to: 'tasks#answer'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create', as: :create_task
+
 end
