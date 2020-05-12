928ca1ab0047d31b31deca6204d92886329d379d
Morgan Lizop <morgan.lizop@iese.net>
2020-05-12 16:12:26 +0100

    user can now update and delete a task

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create', as: :create_task
-
+  patch 'tasks/:id', to: 'tasks#update', as: :update_task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
